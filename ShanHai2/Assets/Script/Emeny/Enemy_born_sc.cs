c8076848f15ba60acfa89667e22e8f359ef50b8f
KeyZxy <118372186+KeyZxy@users.noreply.github.com>
2025-03-06 17:42:44 +0800

    1

---
+++ ShanHai2/Assets/Script/Emeny/Enemy_born_sc.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy_born_sc : MonoBehaviour
+{
+
+    public Text born_text;
+    public GameObject Enemy;
+    public float interval = 2f;
+
+    private bool isStop = true;
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Key_Check();
+        if(!isStop)
+        {
+            timer += Time.deltaTime;
+
+            // 当计时器超过生成间隔时
+            if (timer >= interval)
+            {
+                SpawnEnemy(); // 生成敌人
+                timer = 0f; // 重置计时器
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(Enemy, transform.position, Quaternion.identity , GameObject.Find("Enemy_group").transform);
+    }
+
+
+    void Key_Check()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            if(isStop)
+            {
+                isStop = false;
+                born_text.text = "F2刷怪-开启";
+            }else
+            {
+                isStop = true;
+                born_text.text = "F2刷怪-关闭";
+            }
+        }
+    }
+}
---
+++ ShanHai2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ENV_AMBIENCE_FOREST_BED = 3938107231U;
+        static const AkUniqueID ENV_OBJECT_BIRDS = 396869397U;
+        static const AkUniqueID MUSIC_PLAY = 202194903U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUND_MATERIALS
+        {
+            static const AkUniqueID GROUP = 404567067U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace GROUND_MATERIALS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID ENCIRONMENT = 2592748785U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
